cc94a8e61f4d5296ab40fef8f96d0f629af6ee00
Bahadır <82715842+bahadirAsilsoy@users.noreply.github.com>
2024-11-22 13:02:11 +0300

    update

--- 7.Course-App/src/App.jsx
+++ 7.Course-App/src/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import { courses } from './Data'
-import Courses from './components/Courses'
+import Courses from './components/Course'
+import './css/Course.css';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <div className='course-main'>
       {
         courses?.map((courses)=>(
           <Courses key={courses.id} course={courses}/>
         ))
       }
+      </div>
     </>
   )
 }
