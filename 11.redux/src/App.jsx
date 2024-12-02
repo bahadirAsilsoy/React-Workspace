de85754e51afdde09f371b2bb5e82bab6c445cb0
Bahadır <82715842+bahadirAsilsoy@users.noreply.github.com>
2024-12-02 14:43:16 +0300

    new folder

---
+++ 11.redux/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useSelector, useDispatch} from 'react-redux'
+import { decrement, increment } from './redux/counterSlice'
+
+
+
+function App() {
+
+  const dispatch = useDispatch();
+
+  const { value } = useSelector((store) => store.counter);
+  console.log(value)
+
+  return (
+    <>
+      <div>{value}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>Arttır</button>
+        <button onClick={() => dispatch(decrement())}>Azalt</button>
+      </div>
+    </>
+  )
+}
+
+export default App
