a22e81581c1986b24cea2b669a04a63825e1ba06
Bahadır <82715842+bahadirAsilsoy@users.noreply.github.com>
2024-12-05 15:26:57 +0300

    new project

---
+++ 13.E-Commerce/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import '../css/Header.css'
+import { CiShoppingBasket } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
+import { CiDark } from "react-icons/ci";
+
+function Header() {
+
+  const [theme, setTheme] = useState(false);    
+
+  const changeTheme = () => {
+    const root = document.getElementById("root");
+    if(theme) {
+        root.style.backgroundColor = "black";
+        root.style.color="#fff";
+    } else {
+        root.style.backgroundColor = "#fff";
+        root.style.color="black";
+    }
+    setTheme(!theme);
+  }
+
+  return (
+    <div style={{display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-between"}}>
+        <div className='flex-row'>
+            <img className='logo' src="./src/images/logo.png" alt="" />
+            <p className='logo-text'>BAHO A.Ş.</p>
+        </div>
+
+        <div className='flex-row'>
+            <input className="search-input" type='text' placeholder='Bir şeyler ara'/>
+            <div>
+                {
+                    theme ? <CiDark className='icon' onClick={changeTheme}/> : <CiLight className='icon' onClick={changeTheme}/>
+                }
+                <CiShoppingBasket className='icon'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
